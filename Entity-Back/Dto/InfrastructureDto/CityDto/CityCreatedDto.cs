f5da6c6122a4159dac28545579d362be392004c3
Artunduaga14 <artunduagajuan21@gmail.com>
2025-08-26 06:41:07 -0500

    subo validaciones de infraestructura

--- Entity-Back/Dto/InfrastructureDto/CityDto/CityCreatedDto.cs
+++ Entity-Back/Dto/InfrastructureDto/CityDto/CityCreatedDto.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace Entity_Back.Dto.InfrastructureDto.CityDto
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CityCreatedDto
     {
+        [Required(ErrorMessage = "El departamento es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar un departamento válido")]
         public int DepartamentId { get; set; }
-        public string Name { get; set; }
-       
 
+        [Required(ErrorMessage = "El nombre de la ciudad es obligatorio")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre de la ciudad debe tener entre 2 y 100 caracteres")]
+        public string Name { get; set; }
     }
+
 }
