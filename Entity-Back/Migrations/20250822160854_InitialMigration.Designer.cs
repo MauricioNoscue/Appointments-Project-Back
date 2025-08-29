4eeccee825df10cd8598309c8f5df9b32b233060
esteban11PM <epalomar280@gmail.com>
2025-08-28 21:38:17 -0500

    carpetas de migraciones

--- Entity-Back/Migrations/20250822160854_InitialMigration.Designer.cs
+++ Entity-Back/Migrations/20250822160854_InitialMigration.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Entity_Back.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250821154310_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20250822160854_InitialMigration")]
+    partial class InitialMigration
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
