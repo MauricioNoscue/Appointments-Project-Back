09a941e45ab6f902e5af660f8b4e6123f59eb965
Artunduaga14 <artunduagajuan21@gmail.com>
2025-09-02 11:11:50 -0500

    despues de tato tiempo porfin termino consultorio sjsjs

--- Entity-Back/Dto/HospitalDto/ConsultingRoomDto/ConsultingRoomListDto.cs
+++ Entity-Back/Dto/HospitalDto/ConsultingRoomDto/ConsultingRoomListDto.cs
@@ -7,5 +7,6 @@ namespace Entity_Back
         public string Name { get; set; } = string.Empty;
         public int RoomNumber { get; set; }
         public int Floor { get; set; }
+        public int? BranchId { get; set; }
     }
 }
